0726f335672469aefee63b67960310cd21775499
Larry Gilbert <larry@L2G.to>
2013-09-20 14:25:01 -0700

    riak: download and build from source

--- riak.rb
+++ riak.rb
@@ -2,15 +2,16 @@ require 'formula'
 
 class Riak < Formula
   homepage 'http://basho.com/riak/'
-  url 'http://s3.amazonaws.com/downloads.basho.com/riak/1.4/1.4.2/osx/10.8/riak-1.4.2-OSX-x86_64.tar.gz'
-  version '1.4.2'
-  sha256 '2accc58a0ea2f7bd3edc31c934edb0bff6a1535994607fd6cec9c6bbefcf2abf'
+  url 'http://s3.amazonaws.com/downloads.basho.com/riak/1.4/1.4.2/riak-1.4.2.tar.gz'
+  sha256 '6a1fdcfc1f3f0357eeb377ead6638db4187379e3b40121cef16b517e03c6fd11'
 
-  depends_on :macos => :mountain_lion
-  depends_on :arch => :x86_64
+  depends_on 'erlang-r15' # R16 is too new for 1.4.2
 
   def install
-    prefix.install Dir['*']
+    ENV.deparallelize
+    system 'make rel'
+
+    prefix.install Dir['rel/riak/*']
     inreplace Dir["#{lib}/env.sh"] do |s|
       s.change_make_var! "RUNNER_BASE_DIR", prefix
     end
