10f6474342f45c95b4faff920bad8cdf45383309
Larry Gilbert <larry@L2G.to>
2013-09-20 18:29:12 -0700

    riak: Add plist for running daemon with launchd

--- riak.rb
+++ riak.rb
@@ -7,6 +7,12 @@ class Riak < Formula
 
   depends_on 'erlang-r15' # R16 is too new for 1.4.2
 
+  # Constants for use in this formula's plist
+  SOFT_FILE_LIMIT = 4096
+  START_CMD       = %w( /usr/local/bin/riak start )
+
+  plist_options :manual => START_CMD.join(' ')
+
   def install
     ENV.deparallelize
     system 'make rel'
@@ -16,4 +22,26 @@ class Riak < Formula
       s.change_make_var! "RUNNER_BASE_DIR", prefix
     end
   end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd" >
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          #{START_CMD.map { |s| "<string>#{s}</string>" }}
+        </array>
+        <key>SoftResourceLimits</key>
+        <dict>
+          <key>NumberOfFiles</key>
+          <integer>#{SOFT_FILE_LIMIT}</integer>
+        </dict>
+      </dict>
+    </plist>
+    EOS
+  end
+
 end
