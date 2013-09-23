644a1a8952b99757c54cff349d98f4735988434c
Larry Gilbert <larry@l2g.to>
2013-09-22 21:39:30 -0700

    riak: copy from main Homebrew

---
+++ riak.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Riak < Formula
+  homepage 'http://basho.com/riak/'
+  url 'http://s3.amazonaws.com/downloads.basho.com/riak/1.4/1.4.2/osx/10.8/riak-1.4.2-OSX-x86_64.tar.gz'
+  version '1.4.2'
+  sha256 '2accc58a0ea2f7bd3edc31c934edb0bff6a1535994607fd6cec9c6bbefcf2abf'
+
+  depends_on :macos => :mountain_lion
+  depends_on :arch => :x86_64
+
+  def install
+    prefix.install Dir['*']
+    inreplace Dir["#{lib}/env.sh"] do |s|
+      s.change_make_var! "RUNNER_BASE_DIR", prefix
+    end
+  end
+end
