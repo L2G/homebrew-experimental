3afb10c3cc7bc039222bbfafba2a2c7935bcd3a6
Larry Gilbert <larry@l2g.to>
2013-09-22 21:13:10 -0700

    qt5: update to 5.1.1, comment out bottled stuff

--- qt5.rb
+++ qt5.rb
@@ -2,30 +2,33 @@ require 'formula'
 
 class Qt5 < Formula
   homepage 'http://qt-project.org/'
-  url 'http://download.qt-project.org/official_releases/qt/5.1/5.1.0/single/qt-everywhere-opensource-src-5.1.0.tar.gz'
-  sha1 '12d706124dbfac3d542dd3165176a978d478c085'
+  url 'http://download.qt-project.org/official_releases/qt/5.1/5.1.1/single/qt-everywhere-opensource-src-5.1.1.tar.xz'
+  sha1 'e09a34ef5a80b98eda4e07dcf810d1ce0ef62346'
 
-  bottle do
-    revision 1
-    sha1 '559797c1240c758aea1755b664fb898d492fca03' => :mountain_lion
-    sha1 '67d969a4a260f4576f3fcaf5e1cef23edfd35177' => :lion
-    sha1 '61cfa853784d2493ffa00b3e2897f6f46df5815f' => :snow_leopard
-  end
+#  bottle do
+#    revision 1
+#    sha1 '559797c1240c758aea1755b664fb898d492fca03' => :mountain_lion
+#    sha1 '67d969a4a260f4576f3fcaf5e1cef23edfd35177' => :lion
+#    sha1 '61cfa853784d2493ffa00b3e2897f6f46df5815f' => :snow_leopard
+#  end
 
   head 'git://gitorious.org/qt/qt5.git', :branch => 'stable'
 
   keg_only "Qt 5 conflicts Qt 4 (which is currently much more widely used)."
 
   option :universal
-  option 'with-docs', 'Build documentation'
   option 'developer', 'Build and link with developer options'
 
   depends_on "d-bus" => :optional
   depends_on "mysql" => :optional
 
+  # Renamed option
   odie 'qt5: --with-qtdbus has been renamed to --with-d-bus' if ARGV.include? '--with-qtdbus'
-  odie 'qt5: --with-demos-examples is no longer supported' if ARGV.include? '--with-demos-examples'
-  odie 'qt5: --with-debug-and-release is no longer supported' if ARGV.include? '--with-debug-and-release'
+
+  # Retired options
+  %w( --with-demos-examples --with-debug-and-release --with-docs ).each do |o|
+    odie "qt5: #{o} is no longer supported" if ARGV.include? o
+  end
 
   def install
     ENV.universal_binary if build.universal?
@@ -34,9 +37,6 @@ class Qt5 < Formula
             "-confirm-license", "-opensource",
             "-nomake", "examples",
             "-release"]
-
-    # In latest head `-nomake demos` is no longer recognized
-    args << "-nomake" << "demos" unless build.head?
 
     unless MacOS::CLT.installed?
       # ... too stupid to find CFNumber.h, so we give a hint:
@@ -53,10 +53,6 @@ class Qt5 < Formula
       args << "-I#{dbus_opt}/include/dbus-1.0"
       args << "-L#{dbus_opt}/lib"
       args << "-ldbus-1"
-    end
-
-    unless build.with? 'docs'
-      args << "-nomake" << "docs"
     end
 
     if MacOS.prefer_64_bit? or build.universal?
