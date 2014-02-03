a33c44e159df64868510da44d02888113bd1558b
Larry Gilbert <larry@l2g.to>
2014-02-02 20:27:58 -0800

    bitcoin: test phase will run Bitcoin self-tests

--- bitcoin.rb
+++ bitcoin.rb
@@ -61,12 +61,12 @@ class Bitcoin < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test bitcoin`.
-    system "false"
+    if which('test_bitcoin')
+      system 'test_bitcoin'
+      system 'test_bitcoin-qt' if which('test_bitcoin-qt')
+    else
+      opoo 'Bitcoin tests were not built (0.8.x does not build self-tests)'
+    end
   end
 
 end
