f72a458bc25c3596afa37948598021f44256f119
Larry Gilbert <larry@l2g.to>
2014-02-05 14:13:09 -0800

    Rename iterm -> iterm2 and update with latest version
    I can't seem to figure out the right way to indicate that one
    formula is superseding another. :-(

--- iterm.rb
+++ iterm.rb
@@ -1,36 +1,22 @@
 require 'formula'
 
 class Iterm < Formula
-  homepage ''
-  url 'https://github.com/gnachman/iTerm2/archive/v1.0.0.20131228.tar.gz'
-  version '1.0.0.20131228'
+  homepage 'http://iterm2.com/'
+  url 'file:'
+  version '999'
   sha1 '36eaf9d38c20ffbb4c68502003933c05c3206b43'
 
-  head 'https://github.com/gnachman/iTerm2.git'
-  depends_on :macos => :mountain_lion if build.head?
-
-  # depends_on 'cmake' => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
+  OBSOLESCENCE_MESSAGE = <<-END.undent
+    The iterm formula is obsolete; please use iterm2 instead.
+  END
+    
+  conflicts_with 'iterm2', :because => OBSOLESCENCE_MESSAGE
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    system 'make', 'Deployment'
-    prefix.install 'build/Deployment/iTerm.app'
+    opoo OBSOLESCENCE MESSAGE
   end
 
   def caveats
-    'HEAD builds have not been tested.'
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test iterm`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "--version"`.
-    system "false"
+    OBSOLESCENCE_MESSAGE
   end
 end
---
+++ iterm2.rb
@@ -0,0 +1,37 @@
+require 'formula'
+
+class Iterm2 < Formula
+  homepage 'http://iterm2.com/'
+
+  url 'https://github.com/gnachman/iTerm2/archive/v1.0.0.20140112.tar.gz'
+  sha1 'ac60c6fd9e0c456229113c615f89b494bfd09458'
+
+  head 'https://github.com/gnachman/iTerm2.git'
+
+  conflicts_with 'iterm', :because => 'This supersedes the "iterm" brew.'
+
+  depends_on :macos => (build.head? ? :mountain_lion : :snow_leopard)
+  depends_on :x11
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system 'make', 'Deployment'
+    prefix.install 'build/Deployment/iTerm.app'
+  end
+
+  def caveats
+    'HEAD builds have not been tested.'
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test iterm`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "--version"`.
+    system "false"
+  end
+end
