4bd3e3bb24a4be72b74cd105e80edbcb1e221846
Larry Gilbert <larry@l2g.to>
2015-04-30 22:16:47 -0700

    crafty: edit for brew audit --strict

--- crafty.rb
+++ crafty.rb
@@ -2,26 +2,24 @@
 # by Lawrence Leonard Gilbert (13 Jul 2013)
 # dedicated to the public domain
 #
-# TODO:
+# To do:
 # * Install the opening-moves book
 # * Install help
 # * Determine number of cores and configure appropriately
 
-require 'formula'
-
 class Crafty < Formula
   # Look for newer versions at http://www.cis.uab.edu/hyatt/crafty/source/?C=M;O=D
   # because the home page may not be up-to-date
-  homepage 'http://www.craftychess.com/'
-  url 'http://www.cis.uab.edu/hyatt/crafty/source/crafty-23.8.zip'
-  sha1 '662ae1f97727eacfae0cff0a8e6449a45178648d'
+  homepage "http://www.craftychess.com/"
+  url "http://www.cis.uab.edu/hyatt/crafty/source/crafty-23.8.zip"
+  sha256 "01c3c34125e43dcdba9015d8b31f0e2c46a58d792e8eefc0b75ca3ec6b294e14"
 
   def install
-    system 'make', 'darwin'
-    bin.install 'crafty'
+    system "make", "darwin"
+    bin.install "crafty"
   end
 
   test do
-    system 'crafty end'
+    system "crafty", "end"
   end
 end
