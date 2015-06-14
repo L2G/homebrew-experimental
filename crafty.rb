e01cd7a26409afda7a84299198a0c9765b020413
Larry Gilbert <larry@l2g.to>
2015-06-14 12:31:14 -0700

    crafty: add desc

--- crafty.rb
+++ crafty.rb
@@ -9,6 +9,7 @@
 class Crafty < Formula
   # Look for newer versions at http://www.cis.uab.edu/hyatt/crafty/source/?C=M;O=D
   # because the home page may not be up-to-date
+  desc "A mature, high-performance chess engine; can be used alone or with Xboard"
   homepage "http://www.craftychess.com/"
   url "http://www.cis.uab.edu/hyatt/crafty/source/crafty-24.1.zip"
   sha256 "a29d25d9a26a5a958f07a075d1f76f52c12d287ad16ee7aadb224a0dfee40659"
